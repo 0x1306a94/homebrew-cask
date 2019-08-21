76b3991f65097a448a6712a40d86bdfc00a99941
0x1306a94 <0x1306a94@gmail.com>
2019-08-21 17:02:32 +0800

    去除 xcode-clang-format sha256 验证

--- Casks/xcode-clang-format.rb
+++ Casks/xcode-clang-format.rb
@@ -1,6 +1,6 @@
 cask 'xcode-clang-format' do 
 	version '1.0'
-  	sha256 'e214bb0925614ac7c67dd7af5abe328a2eeba26da0f7076ab4dd9fe9a987e8e9'
+  	sha256 :no_check
 
   	# github.com/atom/atom was verified as official when first introduced to the cask
   	url "https://github.com/0x1306a94/homebrew-apps/blob/master/XcodeClangFormat.app.zip"
