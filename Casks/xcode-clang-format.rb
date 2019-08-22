7ce38d62691e0e0b7901ede7de92ba39018abb90
0x1306a94 <0x1306a94@gmail.com>
2019-08-22 19:02:32 +0800

    Update xcode-clang-format.rb

--- Casks/xcode-clang-format.rb
+++ Casks/xcode-clang-format.rb
@@ -1,6 +1,6 @@
 cask 'xcode-clang-format' do 
 	version '1.1'
-  	sha256 'ae3b6f3c162bcd16ca383a9a5ab26eecf51d190873a054866501db6e96459e11'
+  	sha256 '33527aa74e6cd62656f7bb41024dab762669304a36d04447ca9b8a7ea564824d'
 
   	# github.com/atom/atom was verified as official when first introduced to the cask
   	url "https://raw.githubusercontent.com/0x1306a94/homebrew-apps/master/XcodeClangFormat-v1.1.zip"
